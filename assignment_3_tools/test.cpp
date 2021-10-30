79925d8edc9daeadd5353ac940686b90fef492ac
CameronLeber <90150753+CameronLeber@users.noreply.github.com>
2021-10-30 15:57:14 -0700

    Create test.cpp

---
+++ assignment_3_tools/test.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <cstring>
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	string silent = "silent";
+	string confess = "confess";
+
+	int last = 0;
+
+	stack<string> emotionalStack;
+
+	emotionalStack.push(" ");
+	emotionalStack.push(" ");
+	emotionalStack.push(" ");
+
+	if (strcmp(argv[0], "silent") == 0)
+	{
+		emotionalStack.pop();
+		emotionalStack.push(confess);
+		cout << emotionalStack.top();
+	}
+	else
+	{
+		emotionalStack.pop();
+		emotionalStack.push(silent);
+		cout << emotionalStack.top();
+	}
+	
+
+	return 0;
+
+}
